647ef4d133cb242d7e9ef2dd1fedc0e798b0481e
esratokuc <58563082+esratokuc@users.noreply.github.com>
2025-06-09 00:29:19 +0300

    Update audio_utils.py

--- audio_utils.py
+++ audio_utils.py
@@ -1,6 +1,6 @@
 from moviepy.editor import VideoFileClip
 import os
-import imageio_ffmpeg as ffmpeg  # sadece ffmpeg PATH'ini kaydettirmek için
+import imageio_ffmpeg
 
 def extract_audio(video_path, output_audio="audio.wav"):
     clip = VideoFileClip(video_path)
@@ -9,8 +9,7 @@ def extract_audio(video_path, output_audio="audio.wav"):
 
     output_path = os.path.join("/tmp", output_audio)
 
-    # MoviePy'nin ffmpeg'e erişebilmesi için path belirle (bazı Streamlit makinelerinde gerekli)
-    os.environ["IMAGEIO_FFMPEG_EXE"] = ffmpeg.get_ffmpeg_exe()
+    os.environ["IMAGEIO_FFMPEG_EXE"] = imageio_ffmpeg.get_ffmpeg_exe()
 
     clip.audio.write_audiofile(output_path, codec="pcm_s16le")
     return output_path
