e6625f1e8f7e9ad6cbdccb618ee7982c617f7d0e
esratokuc <58563082+esratokuc@users.noreply.github.com>
2025-06-09 00:36:51 +0300

    Update audio_utils.py

--- audio_utils.py
+++ audio_utils.py
@@ -8,8 +8,6 @@ def extract_audio(video_path, output_audio="audio.wav"):
         raise ValueError("No audio track found in video.")
 
     output_path = os.path.join("/tmp", output_audio)
-
     os.environ["IMAGEIO_FFMPEG_EXE"] = imageio_ffmpeg.get_ffmpeg_exe()
-
     clip.audio.write_audiofile(output_path, codec="pcm_s16le")
     return output_path
