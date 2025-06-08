b4a4723d35c741cd22db0ef3cc0f1a1c151ca46a
esratokuc <58563082+esratokuc@users.noreply.github.com>
2025-06-09 01:42:15 +0300

    Update audio_utils.py

--- audio_utils.py
+++ audio_utils.py
@@ -1,7 +1,1 @@
 
-from moviepy.editor import VideoFileClip
-
-def extract_audio(video_path, output_audio="extracted_audio.wav"):
-    clip = VideoFileClip(video_path)
-    clip.audio.write_audiofile(output_audio)
-    return output_audio
