4358555d1772033d7ff94651b4bb74639cb0ed23
esratokuc <58563082+esratokuc@users.noreply.github.com>
2025-06-09 00:21:12 +0300

    Update audio_utils.py

--- audio_utils.py
+++ audio_utils.py
@@ -4,8 +4,8 @@ import os
 def extract_audio(video_path, output_audio="audio.wav"):
     clip = VideoFileClip(video_path)
     if not clip.audio:
-        raise ValueError("No audio track found in the video.")
-    
-    temp_path = os.path.join("/tmp", output_audio)
-    clip.audio.write_audiofile(temp_path, codec="pcm_s16le")  # WAV format
-    return temp_path
+        raise ValueError("No audio track found in video.")
+
+    output_path = os.path.join("/tmp", output_audio)
+    clip.audio.write_audiofile(output_path, codec="pcm_s16le")
+    return output_path
