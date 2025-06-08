7c7f92f77daa53848f6ee3a3f7122ceaf57aee31
esratokuc <58563082+esratokuc@users.noreply.github.com>
2025-06-09 01:36:03 +0300

    Update audio_utils.py

--- audio_utils.py
+++ audio_utils.py
@@ -1,16 +1,7 @@
+
 from moviepy.editor import VideoFileClip
-from pydub import AudioSegment
-import os
 
-def extract_audio(video_path, output_audio="audio.wav"):
-    video = VideoFileClip(video_path)
-    audio_path = "temp_audio.mp3"
-    video.audio.write_audiofile(audio_path)
-
-    # Convert to WAV using pydub
-    sound = AudioSegment.from_file(audio_path)
-    wav_path = "converted_audio.wav"
-    sound.export(wav_path, format="wav")
-
-    os.remove(audio_path)
-    return wav_path
+def extract_audio(video_path, output_audio="extracted_audio.wav"):
+    clip = VideoFileClip(video_path)
+    clip.audio.write_audiofile(output_audio)
+    return output_audio
