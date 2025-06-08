cb57e76b3c9c290c0a6e3c5691d0402c5a1065e5
esratokuc <58563082+esratokuc@users.noreply.github.com>
2025-06-09 00:15:50 +0300

    Update audio_utils.py

--- audio_utils.py
+++ audio_utils.py
@@ -1,9 +1,13 @@
 from moviepy.editor import VideoFileClip
-from pydub import AudioSegment
 import os
 
 def extract_audio(video_path, output_audio="audio.wav"):
-    clip = VideoFileClip(video_path)
-    audio = clip.audio
-    audio.write_audiofile(output_audio, logger=None)
-    return output_audio
+    try:
+        clip = VideoFileClip(video_path)
+        if not clip.audio:
+            raise ValueError("No audio track found in video.")
+        clip.audio.write_audiofile(output_audio, codec='pcm_s16le')
+        return output_audio
+    except Exception as e:
+        print("Error extracting audio:", e)
+        raise FileNotFoundError("Audio extraction failed.")
