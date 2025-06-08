ce1111c8e23cc065526f085b698d7dfba5424ad2
esratokuc <58563082+esratokuc@users.noreply.github.com>
2025-06-09 01:29:13 +0300

    Update audio_utils.py

--- audio_utils.py
+++ audio_utils.py
@@ -2,6 +2,7 @@ from moviepy.editor import VideoFileClip
 import os
 
 def extract_audio(video_path, output_audio="audio.wav"):
+    audio_path = os.path.splitext(video_path)[0] + ".wav"
     clip = VideoFileClip(video_path)
-    clip.audio.write_audiofile(output_audio, codec='pcm_s16le')
-    return output_audio
+    clip.audio.write_audiofile(audio_path, codec='pcm_s16le')
+    return audio_path
